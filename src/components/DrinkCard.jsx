a770ad3ea5436353b23d1379bf5bdfaef46e429b
Caio-Kaefer <kaefer.caio@gmail.com>
2023-12-30 16:51:56 -0300

    correcao na chamada da api para adicionar favoritos

--- src/components/DrinkCard.jsx
+++ src/components/DrinkCard.jsx
@@ -1,29 +1,44 @@
 import React, { useState } from "react";
 import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
+import { Link } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import axios from "axios";
+import StarIcon from "@mui/icons-material/Star";
 import Button from "@mui/material/Button";
-import "./DrinkCard.css"; 
-import { Dialog, DialogContent, DialogActions } from "@mui/material";
+import Dialog from "@mui/material/Dialog";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
 
 function DrinkCard({ drink }) {
+  const token = localStorage.getItem("token");
+  const decoded = jwt_decode(token);
+  const userId = decoded.id;
+  const [clicked, setClicked] = useState(false);
   const [openDialog, setOpenDialog] = useState(false);
   const [dialogContent, setDialogContent] = useState("");
 
   const handleFavorite = async (id) => {
     try {
-      
-      if (id % 2 === 0) {
-        setDialogContent("Este drink já está marcado como favorito.");
-        setOpenDialog(true);
-      } else {
-        setDialogContent("Drink adicionado aos favoritos!");
-        setOpenDialog(true);
-      }
+      const response = await axios.post(
+        "https://localhost:7062/api/Favorite/addfavorite",
+        {
+          userId: userId,
+          drinkId: id,
+        }
+      );
+      // Se a requisição for bem-sucedida, atualize o estado do ícone
+      setClicked(!clicked);
     } catch (error) {
       console.error("Erro ao adicionar aos favoritos:", error);
-      setDialogContent("Erro ao adicionar aos favoritos.");
-      setOpenDialog(true);
+      if (error.response && error.response.status === 400) {
+        // Se for um erro de bad request (status 400), exibe o diálogo com a mensagem de erro
+        setDialogContent("Este drink já está marcado como favorito.");
+        setOpenDialog(true);
+      }
     }
   };
 
@@ -32,35 +47,42 @@ function DrinkCard({ drink }) {
   };
 
   return (
-    <div className="drink-card">
-      <Card>
-        <CardContent>
-          <img
-            className="drink-image"
-            src={drink.strDrinkThumb}
-            alt={drink.strDrink}
-          />
+    <div style={{ margin: "10px" }}>
+      <Card sx={{ maxWidth: 200 }}>
+        <CardMedia
+          sx={{ height: 300 }}
+          image={drink.strDrinkThumb}
+          title={drink.strDrink}
+        />
+        <CardContent style={{ paddingBottom: "16px" }}>
           <Typography gutterBottom variant="h5" component="div">
             {drink.strDrink}
           </Typography>
           <Typography variant="body2" color="text.secondary">
             {drink.strInstructions}
           </Typography>
+        </CardContent>
+        <CardActions
+          style={{
+            paddingTop: "0",
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <Link to={`/details/${drink.idDrink}`}>Saiba mais</Link>
           <Button onClick={() => handleFavorite(drink.idDrink)}>
             Adicionar aos Favoritos
           </Button>
-        </CardContent>
+        </CardActions>
       </Card>
-      <div>
-        <Dialog open={openDialog} onClose={handleCloseDialog}>
-          <DialogContent>{dialogContent}</DialogContent>
-          <DialogActions>
-            <Button onClick={handleCloseDialog} autoFocus>
-              Fechar
-            </Button>
-          </DialogActions>
-        </Dialog>
-      </div>
+      <Dialog open={openDialog} onClose={handleCloseDialog}>
+        <DialogContent>{dialogContent}</DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDialog} autoFocus>
+            Fechar
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
